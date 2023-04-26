dc43d42b0d23ed7d3c398183a5c8983e491f9c5a
mohsinrashid64 <71118112+mohsinrashid64@users.noreply.github.com>
2023-04-26 15:21:50 +0500

    Added Map Function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 // import { Component } from 'react';
-import { Component } from 'react';
+// import { Component } from 'react';
 import './App.css';
-
+import information from "./information"
 import Heading from "./Components/heading"
 import Card from "./Components/card"
 
-
+function CreateCard(informationOfCard){
+  return (<Card 
+               key={informationOfCard.id}
+               name={informationOfCard.name}
+               image={informationOfCard.image}
+               phone={informationOfCard.phone}
+               email={informationOfCard.email}
+  />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <Heading/>
-      <Card name="Schenider" image="https://picsum.photos/200" phone="03334545454" email="abc@gmail.com"/>
-      <Card name="Genral" image="https://picsum.photos/201" phone="03313421234" email="xyz@outlook.com"/>
-      <Card name="Benz" image="https://picsum.photos/202" phone="04321234321" email="lmn@yahoo.com"/>
+
+      {information.map(CreateCard)}
+
+      <Card name="test" phone="testphone" email="testemail" />
+      
 
 
     </div>
---
+++ src/information.js
@@ -0,0 +1,13 @@
+const information = [
+    {
+        name:"Schenider", image:"https://picsum.photos/200", phone:"03334545454", email:"abc@gmail.com"
+    },
+    {
+        name:"Genral", image:"https://picsum.photos/201", phone:"03313421234", email:"xyz@outlook.com"
+    },
+    {
+        name:"Benz", image:"https://picsum.photos/202", phone:"04321234321", email:"lmn@yahoo.com"
+    }
+  ];
+  
+export default information;
