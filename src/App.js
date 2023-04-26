e53efbb80b3c7119f90be0733bd17cc344136483
mohsinrashid64 <71118112+mohsinrashid64@users.noreply.github.com>
2023-04-26 17:13:13 +0500

    Arrow function

--- src/App.js
+++ src/App.js
@@ -6,67 +6,35 @@ import Heading from "./Components/heading"
 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// OLD WAY
-// function double(param){
-//   return param * 2;
-// }
-// const newNums = numbers.map(double)
-// function double(param){
-//   return param * 2;
-// }
-////////////////////////
 
 
-// Map
-const newNumsMap = numbers.map(function(param){
-  return param * 2;
+const newNumsSquare1 = numbers.map( (param) => { // Note: Brackets around param are only necessary when you want to add multiple parameters
+  return param * param;
 })
 
-// Filter 
-const newNumsFilter = numbers.filter(function(param){
-  return param < 5
+const newNumsSquare2 = numbers.map( param => {
+  return param * param;
 })
 
-
-// Reduce
-const newNumsReduce = numbers.reduce(function(accumulator,param){
-  return accumulator + param
-})
-
-// Find
-const newNumsFind = numbers.find(function(param){
-  return  param > 5
-})
-
-// Find Index
-const newNumsFindIndex = numbers.findIndex(function(param){
-  return  param > 2
-})
+const newNumsSquare3 = numbers.map( param =>  param * param)
 
 function App() {
   return (
     <div className="App">
       <Heading/>
       <p>
-        Map Function: 
-        {newNumsMap + " "}
+        Arrow Function 1: 
+        {" " + newNumsSquare1 + " "}
       </p>
       <p>
-        Filter Function:
-        {newNumsFilter + " "}
+        Arrow Function 2: 
+        {" " + newNumsSquare2 + " "}
       </p>
       <p>
-        Reduce Function: 
-        {newNumsReduce + " "}
+        Arrow Function 3: 
+        {" " + newNumsSquare3 + " "}
       </p>
-      <p>
-        Find Function: 
-        {newNumsFind + " "}
-      </p>
-      <p>
-        Find Index Function: 
-        {newNumsFindIndex + " "}
-      </p>
+
     </div>
   );
 }
