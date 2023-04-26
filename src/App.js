8a84ae6fe778fe52fff1b15d4197b6e72640771e
mohsinrashid64 <71118112+mohsinrashid64@users.noreply.github.com>
2023-04-26 15:04:06 +0500

    Card Creation Practise
    Carde Creation Practise

--- src/App.js
+++ src/App.js
@@ -11,18 +11,9 @@ function App() {
   return (
     <div className="App">
       <Heading/>
-      <List/>
-      {/* <ul>
-        <li>{pi}</li>
-        <li>{doublePi()}</li>
-        <li>{triplePi()} </li>
-      </ul> */}
-      {/* OR */}
-      <ul>
-        <li>{pi.default}</li>
-        <li>{pi.doublePi()}</li>
-        <li>{pi.triplePi()} </li>
-      </ul>
+      <Card name="Schenider" image="https://picsum.photos/200" phone="03334545454" email="abc@gmail.com"/>
+      <Card name="Genral" image="https://picsum.photos/201" phone="03313421234" email="xyz@outlook.com"/>
+      <Card name="Benz" image="https://picsum.photos/202" phone="04321234321" email="lmn@yahoo.com"/>
 
 
     </div>
