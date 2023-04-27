c6265864d4dfeda5950ffb4a5aa6d08025197a4c
mohsinrashid64 <71118112+mohsinrashid64@users.noreply.github.com>
2023-04-27 14:10:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,28 @@ import React, { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-
   function increase() {
     setCount(count + 1);
   }
-
   function decrease() {
     setCount(count - 1);
   }
 
+  const now = new Date().toLocaleTimeString();
+  const [time,setTime] = useState(now);
+
+  function updateTime(){
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime)
+  }
   return (
     <div className="container">
       <h1>{count}</h1>
       <button onClick={decrease}>-</button>
       <button onClick={increase}>+</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
+
     </div>
   );
 }
