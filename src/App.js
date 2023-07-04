d7be9f396b174d1fcc74448d7e9d5cf63655af1c
mohsinrashid64 <71118112+mohsinrashid64@users.noreply.github.com>
2023-07-04 11:35:41 +0500

    Revert "2nd Commit"
    This reverts commit 0a6ee1b36139e8f6cffc525a4203c67ddf311ebb.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,39 @@
+// import { Component } from 'react';
 import './App.css';
-import React from 'react';
-import {Link, Route,Routes} from 'react-router-dom'
-import Home from './Components/Home';
-import About from './Components/About';
-import Contact from './Components/Contact';
-import Book from './Components/Book';
-import BookList from './Components/BookList';
-import NotFound from './Components/NotFound';
+import Heading from "./Components/heading.js"
+import List from './Components/list';
+
+
+/////////////////////////////////////////////
+// Importing multiple exports
+// import pi, {doublePi, triplePi} from './Components/math'; // Note: Default Import Name does not Matter. However in the curly brackets name does matter.
+// OR
+import * as pi from "./Components/math";
+/////////////////////////////////////////////
+
+//
 
 
 function App() {
   return (
-
-    <>
-    <nav>
+    <div className="App">
+      {/* <h1>App Page</h1> */}
+      <Heading/>
+      <List/>
+      {/* <ul>
+        <li>{pi}</li>
+        <li>{doublePi()}</li>
+        <li>{triplePi()} </li>
+      </ul> */}
+      {/* OR */}
       <ul>
-        <li>
-          <Link to='/'>Home</Link> 
-        </li>
-        <li>
-          <Link to='/about'>About</Link> 
-        </li>
-        <li>
-          <Link to='/contact'>Contact</Link>  
-        </li>
-        <li>
-          <Link to='/book'>Book List</Link>  
-        </li>
+        <li>{pi.default}</li>
+        <li>{pi.doublePi()}</li>
+        <li>{pi.triplePi()} </li>
       </ul>
-    </nav>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        {/* <Route path="/book" element={<BookList/>}/>
-        <Route path="/book/:id" element={<Book/>}/> */}
-        <Route path="*" element={<NotFound/>}/>
-
-      {/* NESTED ROUTES */}
-      <Route path="/book">
-        <Route index element={<BookList/>}/>
-        <Route path=":id" element={<Book/>}/>
-      </Route>
-      {/* NESTED ROUTES */}
 
 
-      </Routes>
-    </>
-    
+    </div>
   );
 }
 
