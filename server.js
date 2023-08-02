1787ce4c200ac561d862a0a6769acf9c5cb1ac74
mohsinrashid64 <71118112+mohsinrashid64@users.noreply.github.com>
2023-08-02 11:52:21 +0500

    1st commit

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // Use environment variable or default to 5000
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -21,71 +22,41 @@ connection.once('open', () => {
   console.log('Connected to MongoDB Atlas');
 });
 
-// Define the Mongoose schema and model
-const signUpSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String
+// Define the Todo schema
+const todoSchema = new mongoose.Schema({
+  text: String,
+  completed: Boolean,
 });
 
-const SignUpModel = mongoose.model('SignUp', signUpSchema);
+const Todo = mongoose.model('Todo', todoSchema);
 
-app.post('/getsignupdetails', async (req, res) => {
-    const { email } = req.body; // Ensure the 'password' field is in the request body
-  console.log("YO",req.body['name'])
-  console.log("YOoooooooo",typeof(req.body['name']))
-
-  const _user = await SignUpModel.findOne( {email} ); // Find the user by both email and password
-  console.log("USER", _user);
-  if (_user) {
-    console.log("USER ALREADY EXISTS")
-    // res.status(401).json({ message: "User already exists" });
+// API routes
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json('Error fetching todos');
   }
-  else{
-    const newSignupData = new SignUpModel({
-        name:req.body['name'],
-        email:req.body['email'],
-        password:req.body['password'],
-      });
-    
-      try {
-        await newSignupData.save();
-        res.json('Sign Up Data Added!');
-      } catch (err) {
-        console.log(err);
-        res.status(400).json('Error adding todo');
-      }
+});
+
+app.post('/todos/add', async (req, res) => {
+  const newTodo = new Todo({
+    text: req.body.text,
+    completed: false,
+  });
+
+  try {
+    await newTodo.save();
+    res.json('Todo added!');
+  } catch (err) {
+    console.log(err);
+    res.status(400).json('Error adding todo');
   }
-  
 });
 
-
-app.post('/getlogindetails', async (req, res) => {
-    const { email, password } = req.body; // Ensure the 'password' field is in the request body
-    try {
-        const user = await SignUpModel.findOne({ email, password }); // Find the user by both email and password
-        if (user) {
-            console.log("User exists!");
-            // Do something if the user exists
-            // For example, you can send a success response back to the client
-            res.status(200).json({ message: "Login successful!" });
-        } else {
-            console.log("User does not exist!");
-            // Do something if the user does not exist
-            // For example, you can send a failure response back to the client
-            res.status(401).json({ message: "Invalid credentials!" });
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "Internal server error" });
-    }
-});
-
-
-app.get('/sendlogindetails',async(req,res)=>{
-
-});
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import './App.css';
-import React from 'react';
-import {Route,Routes} from 'react-router-dom'
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import Home from './Components/Home';
 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodoText, setNewTodoText] = useState('');
 
-function App() {
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get('/todos');
+      setTodos(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setNewTodoText(e.target.value);
+  };
+
+  const addTodo = async () => {
+    if (!newTodoText) return;
+
+    try {
+      await axios.post('/todos/add', { text: newTodoText });
+      setNewTodoText('');
+      fetchTodos();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-
     <div>
-      <Routes>
-      <Route path='/' element={<Login/>}></Route>  
-      <Route path='/signup' element={<Signup/>}></Route>
-      <Route path='/home' element={<Home/>}></Route>  
-      </Routes>
-      
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        value={newTodoText}
+        onChange={handleInputChange}
+        placeholder="Enter a new todo..."
+      />
+      <button onClick={addTodo}>Add Todo</button>
 
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            {todo.text} - {todo.completed ? 'Completed' : 'Not Completed'}
+          </li>
+        ))}
+      </ul>
     </div>
-    
   );
-}
+};
 
 export default App;
