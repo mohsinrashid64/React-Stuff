56fa26401823c64e0702fb08fd40b846264c7ff9
mohsinrashid64 <71118112+mohsinrashid64@users.noreply.github.com>
2023-10-16 16:27:57 +0500

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
-import './App.css';
 import React from 'react';
-import {Link, Route,Routes} from 'react-router-dom'
-import Home from './Components/Home';
-import About from './Components/About';
-import Contact from './Components/Contact';
-import Book from './Components/Book';
-import BookList from './Components/BookList';
-import NotFound from './Components/NotFound';
-
+import { useFormAction } from 'react-router-dom';
 
 function App() {
+  const action = useFormAction(); // Get the resolved form action
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform form submission using the resolved action
+    console.log('Form submitted with action:', action);
+    // You can make an API request here or perform other actions based on the resolved action.
+  };
+
   return (
-
-    <>
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link> 
-        </li>
-        <li>
-          <Link to='/about'>About</Link> 
-        </li>
-        <li>
-          <Link to='/contact'>Contact</Link>  
-        </li>
-        <li>
-          <Link to='/book'>Book List</Link>  
-        </li>
-      </ul>
-    </nav>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        {/* <Route path="/book" element={<BookList/>}/>
-        <Route path="/book/:id" element={<Book/>}/> */}
-        <Route path="*" element={<NotFound/>}/>
-
-      {/* NESTED ROUTES */}
-      <Route path="/book">
-        <Route index element={<BookList/>}/>
-        <Route path=":id" element={<Book/>}/>
-      </Route>
-      {/* NESTED ROUTES */}
-
-
-      </Routes>
-    </>
-    
+    <div>
+      <form onSubmit={handleSubmit} action={action} method="post">
+        <input type="text" name="data" placeholder="Data" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
 export default App;
-
-
